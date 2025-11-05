ffc9818e3d463be6639c09fdc9071b07ef544365
Alex Fedotov <alxfed@gmail.com>
2025-11-05 08:40:35 -0600

    Update action.mjs

--- assets/js/action.mjs
+++ assets/js/action.mjs
@@ -301,7 +301,7 @@ class MachineApp {
     if (this.settings.llm.token) return true;
     
     try {
-      const tokenResponse = await fetch(`this.settings.machine.server + '/token/' + ${this.settings.machine.token}`);
+      const tokenResponse = await fetch(this.settings.machine.server + '/token/' + this.settings.machine.token, {mode: "cors"});
       if (!tokenResponse.ok) {
         throw new Error(`Server responded with status: ${tokenResponse.status}`);
       }
